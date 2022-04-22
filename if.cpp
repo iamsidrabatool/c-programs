561f64e095181317a2319b037dc429a63a114e12
sidra batool <104225329+iamsidrabatool@users.noreply.github.com>
2022-04-22 12:18:01 -0700

    Create if.cpp

---
+++ if.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	cout<<"Enter the vale of a"<<endl;
+	cin>>a;
+	cout<<"Enter the value of b"<<endl;
+	cin>>b;
+	if(a <= b)
+	{
+		cout<<"a is less than or equal to b"<<endl;
+		
+	}
+	else{
+		cout<<"a is greater than b"<<endl;
+	}
+	cout<<"values of a and b"<<a  <<b<<endl;
+	return 0;
+}
