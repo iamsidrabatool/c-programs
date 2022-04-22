6fd01ccd28000506a45266e02080d6d175e85374
sidra batool <104225329+iamsidrabatool@users.noreply.github.com>
2022-04-22 12:20:18 -0700

    Create nested.cpp

---
+++ nested.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int marks;
+	cout<<"Enter marks"<<endl;
+	cin>>marks;
+	if(marks >=35)
+	{
+		cout<<"you passed "<<endl;
+		if(marks =45)
+		{
+			cout<<"Excellent "<<endl;
+		}
+	}
+	else
+	{
+	cout<<"failed"<<endl;
+}
+	return 0;
+}
